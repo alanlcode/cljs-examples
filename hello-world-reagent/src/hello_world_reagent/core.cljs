f34c8190af830bed34ba07b854af8433e53a49f7
Alan Leung <alanlcode@gmail.com>
2015-09-15 12:37:37 -0700

    Add hello-world-reagent

--- hello-world-reagent/src/hello_world_reagent/core.cljs
+++ hello-world-reagent/src/hello_world_reagent/core.cljs
@@ -1,9 +1,39 @@
 (ns hello-world-reagent.core
-  (:require [clojure.browser.repl :as repl]))
+  (:require [clojure.browser.repl :as repl]
+            [reagent.core :as r]))
 
-;; (defonce conn
-;;   (repl/connect "http://localhost:9000/repl"))
+(defonce app-state
+  (r/atom
+    {:value "Hello World!"}))
+
+(defn output-text [state]
+  [:span (:value state)])
+
+(defn on-change [new-value]
+  (swap! app-state #(assoc % :value new-value)))
+
+(defn text-box []
+  (fn [state]
+    [:input {:type "text"
+             :placeholder "Click here to change message"
+             :value (:value state)
+             :on-change #(on-change (-> % .-target .-value))}]))
+
+(defn ui []
+  [:div
+   [:div
+    [output-text @app-state]]
+   [:div
+    [text-box @app-state]]])
+
+(defn mount []
+  (r/render-component
+    [ui]
+    (.getElementById js/document "root")))
+
+(defonce conn
+  (repl/connect "http://localhost:9000/repl"))
 
 (enable-console-print!)
 
-(println "Hello world!")
+(mount)
